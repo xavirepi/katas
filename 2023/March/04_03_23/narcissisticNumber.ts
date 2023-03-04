a610d887f0672fe4713abd80d4239ab0af98eae2
xavirepi <xavirepi@hotmail.com>
2023-03-04 13:50:30 +0100

    refactor: add year and months folders to better organise katas

--- 2023/March/04_03_23/narcissisticNumber.ts
+++ 2023/March/04_03_23/narcissisticNumber.ts
@@ -1,7 +1,9 @@
 function narcissistic(value: number): boolean {
   const arrFromNumber = value.toString().split("");
   const numberLength = arrFromNumber.length;
-  const poweredNumsArr = arrFromNumber.map(number =>  Math.pow(+number, numberLength));
+  const poweredNumsArr = arrFromNumber.map((number) =>
+    Math.pow(+number, numberLength)
+  );
   const sumOfPowers = poweredNumsArr.reduce((acc, val) => +acc + val, 0);
   return value === sumOfPowers;
 }
